df054c44848f88aaada17b2c9aaf305973222a3d
margo <margo@ubuntu20.04.myguest.virtualbox.org>
2025-02-16 21:27:01 +0300

    init

---
+++ app.js
@@ -0,0 +1,6 @@
+const Agent = require('./agent')
+const VERSION = 7 // Версия сервера
+let teamName = "teamA" // ИМЯ команды
+let agent = new Agent(); // Создание экземпляра агента
+require('./socket')(agent, teamName, VERSION) //Настройка сокета
+agent.socketSend("move", `-15 0`) // Размещение игрока на поле
---
+++ socket.js
@@ -0,0 +1,16 @@
+const dgram = require('dgram')
+module.exports = function(agent, teamName, version){
+    //create socket
+    const socket = dgram.createSocket({type:'udp4',reuseAddr:true})
+    agent.setSocket(socket) //set socket via agent
+    socket.on('message', (msg, info) =>{
+        agent.msgGot(msg) // Обработка полученного сообщения
+    })
+    socket.sendMsg = function (msg){// Отправка сообщения серверу
+        socket.send(Buffer.from(msg), 6000, 'localhost', (err,bytes) =>{
+            if(err) throw err
+        })
+    }
+    // ИНициализация игрока на сервере(без параметра goalie)
+    socket.sendMsg('(init ${teamName} (version ${version}))')
+}
