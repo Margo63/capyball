4d6dab79d3861de2cbeffc8a4c7e33d9a8115481
Khoroshkova Alexandra <kakoy-to_wuk@mail.ru>
2025-03-31 12:33:01 +0300

    fix script

--- src/teamA/teamA.js
+++ src/teamA/teamA.js
@@ -1,4 +1,3 @@
-
 const param1 = process.argv.slice(2);
 
 const Agent = require('../agent/agentGrid')
@@ -6,119 +5,53 @@ const Controller = require('../agent/controller/controller')
 const VERSION = 7 // Версия сервера
 let teamName = "teamA" // Имя команды
 console.log(param1)
+let getControllers = (type) => {
+    const ctrl1_p = require(`../agent/controller/comand/${type}/level_1`)
+    const ctrl2_p = require(`../agent/controller/comand/${type}/level_2`)
+    const ctrl3_p = require(`../agent/controller/comand/${type}/level_3`)
+    return [ctrl1_p, ctrl2_p, ctrl3_p]
+ }
+
 switch (Number(param1[0])) {
-    case 1:{ //goalKeeper
-        const ctrl1 = require('../agent/controller/comand/goalkeeper/level_1')
-        const ctrl2 = require('../agent/controller/comand/goalkeeper/level_2')
-        const ctrl3 = require('../agent/controller/comand/goalkeeper/level_3')
 
-        const DT_GOALKEEPER = require('../agent/controller/condTree/tree/goalKeeperCondTree');
-        let controller1 = new Controller( teamName, [ctrl1, ctrl2, ctrl3], 0);
-        let agent1 = new Agent(controller1); // Создание экземпляра агента-15
-        require('../serverCommunication/socket')(agent1, teamName, VERSION, true)
-        agent1.socketSend("move", `-50 0`)
+    case 0: {//goalKeeper
+        const controllers = getControllers("goalkeeper")
+        let controller2 = new Controller(teamName, controllers, Number(param1[0]));
+        let agent2 = new Agent(controller2);
+        require('../serverCommunication/socket')(agent2, teamName, VERSION, true)
+        agent2.socketSend("move", `-50 0`)
         break;
     }
-    case 2:{
-        const ctrl1_p = require('../agent/controller/comand/player/level_1')
-        const ctrl2_p = require('../agent/controller/comand/player/level_2')
-        const ctrl3_p = require('../agent/controller/comand/player/level_3')
-        let controller2 = new Controller(teamName, [ctrl1_p, ctrl2_p, ctrl3_p], 1);
+    case 1:
+    case 2:
+    case 3: {
+        const controllers = getControllers("centre_defender")
+        let controller2 = new Controller(teamName, controllers, Number(param1[0]));
+        let agent2 = new Agent(controller2);
+        require('../serverCommunication/socket')(agent2, teamName, VERSION)
+        agent2.socketSend("move", `0 0`)
+        break;
+    }
+    case 4:
+    case 5:
+    case 6:
+    case 7: {
+        const controllers = getControllers("defender")
+        let controller2 = new Controller(teamName, controllers, Number(param1[0]));
         let agent2 = new Agent(controller2);
         require('../serverCommunication/socket')(agent2, teamName, VERSION)
         agent2.socketSend("move", `-25 0`)
-
-        break;
+        break
     }
-    case 3:{
-        const ctrl1_p = require('../agent/controller/comand/player/level_1')
-        const ctrl2_p = require('../agent/controller/comand/player/level_2')
-        const ctrl3_p = require('../agent/controller/comand/player/level_3')
-        let controller2 = new Controller(teamName, [ctrl1_p, ctrl2_p, ctrl3_p], 1);
+    case 8:
+    case 9:
+    case 10: {
+        const controllers = getControllers("forward")
+        let controller2 = new Controller(teamName, controllers, Number(param1[0]));
         let agent2 = new Agent(controller2);
         require('../serverCommunication/socket')(agent2, teamName, VERSION)
-        agent2.socketSend("move", `-25 0`)
-        break;
-    }
-    case 4:{
-        const ctrl1_p = require('../agent/controller/comand/player/level_1')
-        const ctrl2_p = require('../agent/controller/comand/player/level_2')
-        const ctrl3_p = require('../agent/controller/comand/player/level_3')
-        let controller2 = new Controller(teamName, [ctrl1_p, ctrl2_p, ctrl3_p], 2);
-        let agent2 = new Agent(controller2);
-        require('../serverCommunication/socket')(agent2, teamName, VERSION)
-        agent2.socketSend("move", `-25 0`)
-        break;
-    }
-    case 5:{
-        const ctrl1_p = require('../agent/controller/comand/player/level_1')
-        const ctrl2_p = require('../agent/controller/comand/player/level_2')
-        const ctrl3_p = require('../agent/controller/comand/player/level_3')
-        let controller2 = new Controller(teamName, [ctrl1_p, ctrl2_p, ctrl3_p], 2);
-        let agent2 = new Agent(controller2);
-        require('../serverCommunication/socket')(agent2, teamName, VERSION)
-        agent2.socketSend("move", `-25 0`)
-        break;
-    }
-    case 6:{
-        const ctrl1_p = require('../agent/controller/comand/player/level_1')
-        const ctrl2_p = require('../agent/controller/comand/player/level_2')
-        const ctrl3_p = require('../agent/controller/comand/player/level_3')
-        let controller2 = new Controller(teamName, [ctrl1_p, ctrl2_p, ctrl3_p], 2);
-        let agent2 = new Agent(controller2);
-        require('../serverCommunication/socket')(agent2, teamName, VERSION)
-        agent2.socketSend("move", `-25 0`)
-        break;
-    }
-    case 7:{
-        const ctrl1_p = require('../agent/controller/comand/player/level_1')
-        const ctrl2_p = require('../agent/controller/comand/player/level_2')
-        const ctrl3_p = require('../agent/controller/comand/player/level_3')
-        let controller2 = new Controller(teamName, [ctrl1_p, ctrl2_p, ctrl3_p], 3);
-        let agent2 = new Agent(controller2);
-        require('../serverCommunication/socket')(agent2, teamName, VERSION)
-        agent2.socketSend("move", `-25 0`)
-        break;
-    }
-    case 8:{
-        const ctrl1_p = require('../agent/controller/comand/player/level_1')
-        const ctrl2_p = require('../agent/controller/comand/player/level_2')
-        const ctrl3_p = require('../agent/controller/comand/player/level_3')
-        let controller2 = new Controller(teamName, [ctrl1_p, ctrl2_p, ctrl3_p], 3);
-        let agent2 = new Agent(controller2);
-        require('../serverCommunication/socket')(agent2, teamName, VERSION)
-        agent2.socketSend("move", `-25 0`)
-        break;
-    }
-    case 9:{
-        const ctrl1_p = require('../agent/controller/comand/player/level_1')
-        const ctrl2_p = require('../agent/controller/comand/player/level_2')
-        const ctrl3_p = require('../agent/controller/comand/player/level_3')
-        let controller2 = new Controller(teamName, [ctrl1_p, ctrl2_p, ctrl3_p], 3);
-        let agent2 = new Agent(controller2);
-        require('../serverCommunication/socket')(agent2, teamName, VERSION)
-        agent2.socketSend("move", `-25 0`)
-        break;
-    }
-    case 10:{
-        const ctrl1_p = require('../agent/controller/comand/player/level_1')
-        const ctrl2_p = require('../agent/controller/comand/player/level_2')
-        const ctrl3_p = require('../agent/controller/comand/player/level_3')
-        let controller2 = new Controller(teamName, [ctrl1_p, ctrl2_p, ctrl3_p], 4);
-        let agent2 = new Agent(controller2);
-        require('../serverCommunication/socket')(agent2, teamName, VERSION)
-        agent2.socketSend("move", `-25 0`)
-        break;
-    }
-    case 11:{
-        const ctrl1_p = require('../agent/controller/comand/player/level_1')
-        const ctrl2_p = require('../agent/controller/comand/player/level_2')
-        const ctrl3_p = require('../agent/controller/comand/player/level_3')
-        let controller2 = new Controller(teamName, [ctrl1_p, ctrl2_p, ctrl3_p], 5);
-        let agent2 = new Agent(controller2);
-        require('../serverCommunication/socket')(agent2, teamName, VERSION)
-        agent2.socketSend("move", `-25 0`)
-        break;
+        agent2.socketSend("move", `25 0`)
+        break
     }
 
 }
